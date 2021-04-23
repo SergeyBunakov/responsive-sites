6d15c0f8e277027e27b7b9cf572df4875e28c41e
SergeyBunakov <bunakov.sergey@gmail.com>
2021-04-23 17:03:22 +0300

    add spinner

--- 07-architect-website/script.js
+++ 07-architect-website/script.js
@@ -1,4 +1,18 @@
-"use strickt";
+  "use strict";
+
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("change");
+  document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector('.scroll-btn').addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehaviotor = "unset";
+  }, 1000);
 });
